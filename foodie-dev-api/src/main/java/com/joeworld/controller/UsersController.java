baab7dccc22277d318e55715574e049668173fb1
SuXiaoo-qiu <87381442@qq.com>
2022-05-21 18:26:22 +0800

    用户登录 注册 购物车中的数据同步到redis和cookie

--- foodie-dev-api/src/main/java/com/joeworld/controller/UsersController.java
+++ foodie-dev-api/src/main/java/com/joeworld/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.joeworld.controller;
 
 import com.joeworld.common.*;
 import com.joeworld.pojo.Users;
+import com.joeworld.pojo.bo.ShopcartBo;
 import com.joeworld.pojo.vo.UserVo;
 import com.joeworld.service.UsersService;
 import io.swagger.annotations.Api;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -26,12 +28,14 @@ import java.util.Map;
 @CrossOrigin //跨域
 /* 类注解 */
 @Api(value = "用户",tags = {"用于登录注册展示的相关用户接口"})
-public class UsersController {
+public class UsersController  extends BaseConller{
+    final static Logger logger = LoggerFactory.getLogger(UsersController.class);
 
     @Autowired
     private UsersService usersService;
+    @Autowired
+    private RedisUtils redisUtils;
 
-    final static Logger logger = LoggerFactory.getLogger(UsersController.class);
     /**
      * 分页查询
      * @param params
@@ -170,6 +174,7 @@ public class UsersController {
         }
         Users user = usersService.createUser(userVo);
         CookieUtils.setCookie(request,response,"user", JsonUtils.objectToJson(user),true);
+        this.synchShopcartData(user.getId(),request,response);
 
         return R.ok(user);
     }
@@ -196,7 +201,73 @@ public class UsersController {
          * isEncode 是否加密 true是  false否
          */
         CookieUtils.setCookie(request,response,"user", JsonUtils.objectToJson(usersRest),true);
+        // 同步购物车数据
+        this.synchShopcartData(usersRest.getId(),request,response);
         return R.ok(usersRest);
+    }
+
+    /**
+     * 注册登录后同步cookie 和redis中的购物车数据
+     *
+     * 1. redis 中如果没有数据  如果cooki中的购车为空 不做任何处理 如果cookie中的购物车不为空那就吧cookie的数据放入redis中
+     * 2。redis 中如果有数据 如果cookie中的购物车不为空 那么就把redis中的数据放入cookie中 覆盖本地的cookie 如果cookie不为空（redis和cookie都不为空） 那就以cookie为主 redis没有的就从cookie添加到redis中
+     * 3. 同步到redis中了以后 覆盖本地cookie 购物车的数据 保证本地购车数据是最新的
+     */
+    private void  synchShopcartData(String userId,HttpServletRequest request, HttpServletResponse response){
+        // 获取redis 购物车中数据
+        String shopcartStrRedis = redisUtils.get(FOODIE_SHOPCART + userId);
+        // 获取cookie 购物车中数据
+        String shopcartStrCookie = CookieUtils.getCookieValue(request, FOODIE_SHOPCART, true);
+        if (StringUtils.isBlank(shopcartStrRedis)){
+            if (StringUtils.isNotBlank(shopcartStrCookie)){
+                redisUtils.set(FOODIE_SHOPCART+userId,shopcartStrCookie);
+            }
+        }else {
+            // redis不为空 cookie也不为空 就合并 （如果同一商品cookie覆盖redis）
+            if (StringUtils.isNotBlank(shopcartStrCookie)) {
+                /**
+                 * 1. 已经存在的 把cookie 中对应的数量 覆盖redis （根据具体需求） 也可以全部覆盖
+                 * 2. 把这个数据标记成待删除 统一放入一个list中
+                 * 3. 从cookie中清理所有待删除的数据
+                 * 4。 合并redis嗯哼cookie中的数据
+                 * 5. 更新到redis和cookie中
+                 */
+                List<ShopcartBo> shopcartBosRedis= JsonUtils.jsonToList(shopcartStrRedis, ShopcartBo.class);
+                List<ShopcartBo> shopcartBosCookie = JsonUtils.jsonToList(shopcartStrCookie, ShopcartBo.class);
+                // 定义一个待删除的list
+                ArrayList<ShopcartBo> shopcartBosDelete = new ArrayList<>();
+
+                for (ShopcartBo shopcartBoRedis : shopcartBosRedis) {
+                    String specIdRedis = shopcartBoRedis.getSpecId();
+                    for (ShopcartBo shopcartBoCookie : shopcartBosCookie) {
+                        String specIdCookie = shopcartBoCookie.getSpecId();
+
+                        if (specIdRedis.equals(specIdCookie)){
+                            //如果redis 和 cookie 中的数据都不为空 那就把redis 中的数据进行覆盖 （把coookie的数量覆盖到redis中）
+                            shopcartBoRedis.setBuyCounts(shopcartBoCookie.getBuyCounts());
+                            // 放入待删除list 等下合并完删除
+                            shopcartBosDelete.add(shopcartBoCookie);
+                        }
+
+                    }
+
+                }
+                //删除cookie已经合并的数据
+                shopcartBosCookie.removeAll(shopcartBosDelete);
+                // 把cookie中剩下数据进行覆盖到redis 这个就是最新的数据
+                shopcartBosRedis.addAll(shopcartBosCookie);
+                // 跟新到cookie 和redis
+                redisUtils.set(FOODIE_SHOPCART+userId,JsonUtils.objectToJson(shopcartBosRedis));
+                CookieUtils.setCookie(request, response,FOODIE_SHOPCART,JsonUtils.objectToJson(shopcartBosRedis),true);
+
+            }else {
+                // redis 不为空  cookie为空 直接覆盖cookie
+                CookieUtils.setCookie(request, response,FOODIE_SHOPCART,shopcartStrRedis,true);
+
+            }
+        }
+
+
     }
 
 
